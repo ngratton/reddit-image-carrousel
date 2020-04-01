ded67d3a0336e82230b7674f63e570b5ace3529c
Nicholas Gratton <nichgratton@gmail.com>
2020-04-01 14:51:09 -0400

    Fonctionnel, mais pas optimisé

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@
  * ##########################
  * 
  * Bug connu: Certaines images hébergées sur IMGUR ne s'affichent pas dans le
- * navigateur par ils ne permettent pas d'être loadé depuis "localhost" ou "127.0.0.1"
+ * navigateur parce qu'ils ne permettent pas d'être loadé depuis "localhost"
  * 
- * Projet en ligne : https://ngratton.github.io/reddit-image-carrousel/
+ * Projet en ligne : https://ngratton.github.io/reddit-images-carrousel/
  * 
  */
 
@@ -27,16 +27,16 @@ let app = new Vue({
         carrousel,
     },
     created() {
+        this.activeImage = './img/loading.gif'
         this.loopSubsList(this.subreddits).then(() => {
-            // let randNumber = Math.floor(Math.random() * this.postsList.length)
-            // this.randomPost(randNumber)
-            
-            console.log(this.postsList)
+            setTimeout(()=> {
+                let randNumber = Math.floor(Math.random() * this.postsList.length)
+                this.randomPost(randNumber)
+            }, 1000)
             this.navigatePostsWithArrows()
         })
     }, // created
     mounted() {
-
     }, // mounted
     methods: {
         loopSubsList(subs) {
@@ -50,11 +50,14 @@ let app = new Vue({
         },
         getPosts(sub) {
             const NONTHUMB = ['nsfw', 'self', 'default']
-            this.fetchSubPosts(sub, []).then(data => {
+            return this.fetchSubPosts(sub, []).then(data => {
+                // console.log(data)
+                
+                // return this.postsList.push(data)
                 data.forEach(post => {
                     post = post.data
                     if(!NONTHUMB.includes(post.thumbnail)) {
-                        this.postsList.push(post)
+                        return this.postsList.push(post)
                     }
                     this.postsList = this.postsList.sort(function() { return 0.5 - Math.random() })
                 })
@@ -76,40 +79,46 @@ let app = new Vue({
             window.addEventListener('keyup', e => {
                 if(e.key === 'ArrowLeft') {
                     let position = this.activePostIndex
-                    if (position > 0) {
-                        position = position - 1
-                        this.selectionPost(position)
-                        this.activePostIndex = position
-                    }
+                    this.naviguerGauche(position)
                 }
                 if(e.key === 'ArrowRight') {
-                    let max = this.postsList.length - 1
                     let position = this.activePostIndex
-                    if(position < max) {
-                        position = position + 1
-                        this.selectionPost(position)
-                        this.activePostIndex = position
-                    }
+                    this.naviguerDroite(position)
                 }
             })
         },  // navigatePostsWithArrows
+        naviguerGauche(position) {
+            if (position > 0) {
+                position = position - 1
+                this.selectionPost(position)
+                this.activePostIndex = position
+            }
+        }, //naviguerGauche
+        naviguerDroite(position) {
+            let max = this.postsList.length - 1
+            if(position < max) {
+                position = position + 1
+                this.selectionPost(position)
+                this.activePostIndex = position
+            }
+        }, //naviguerDroite
         selectionPost(index) {
             this.activePost = this.postsList[index]
             this.activeImage = './img/loading.gif'
             this.activePost.index = index
             this.activePostIndex = index
             this.scrollToActive(index)
-        },
+        }, // selectionPost
         scrollToActive(index) {
             let carrousel = document.querySelector('.scroll-container')
             let halfScreenWidth = carrousel.clientWidth / 2 - 60
             let miniatureWidth = 120 
             let largeurAScroller = (miniatureWidth * index)
             carrousel.style.left = `${0 - largeurAScroller + halfScreenWidth}px`
-        },
+        }, // scrollToActive
         imgLoad() {
             this.activeImage = this.activePost.url
-        }
+        }  // imgLoad
     }, 
 })
 
